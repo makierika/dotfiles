4e790856c36426434220a03c8b775589fef789ca
makierika <makierika01@gmail.com>
2024-10-29 12:52:09 +0800

    fish: add yazi function

--- fish/config.fish
+++ fish/config.fish
@@ -9,9 +9,17 @@ alias c=clear
 alias ff=fastfetch
 alias ls="eza --icons=always"
 alias tree="ls --tree"
-alias yy=yazi
 alias httpd="python -m http.server 80"
 alias v=nvim
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
 
 eval "$(zoxide init fish)"
 eval "$(fzf --fish)"
