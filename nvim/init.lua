e21a83d3c5b050b87014b000956492ddcb98d525
makierika <makierika01@gmail.com>
2024-11-15 18:56:02 +0800

    nvim: customize dashboard ui
    Signed-off-by: makierika <makierika01@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -48,7 +48,15 @@ local plugins = {
   {
     'goolord/alpha-nvim',
     config = function ()
-        require('alpha').setup(require('alpha.themes.dashboard').config)
+      local alpha = require'alpha'
+      local dashboard = require'alpha.themes.dashboard'
+      dashboard.section.buttons.val = {
+        dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+        dashboard.button("SPC f", "󰈞  Find file"),
+        dashboard.button("SPC g", "󰈬  Find word"),
+        dashboard.button( "q", "󰅚  Quit NVIM" , ":qa<CR>"),
+      }
+      alpha.setup(dashboard.config)
     end
   },
   {
