b862f6f2c14a07fa37b6bbfd4e61d0f1fd7b4f81
makierika <makierika01@gmail.com>
2024-12-18 20:30:14 +0800

    add vim-like tmux navigate

--- nvim/init.lua
+++ nvim/init.lua
@@ -107,6 +107,21 @@ local plugins = {
     keys = {
       { '<leader>lg', '<cmd>LazyGit<cr>' }
     }
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight"
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" }
+    }
   }
 }
 local opts = {}
