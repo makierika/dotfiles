c2c729e15d8a757dd30f081d33ae045e8ea13ad6
makierika <makierika01@gmail.com>
2026-05-21 18:16:38 +0800

    some update

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -3,6 +3,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     local opts = { buffer = ev.buf, silent = true }
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'K', vim.diagnostic.open_float, opts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
